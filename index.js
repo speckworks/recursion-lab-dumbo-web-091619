5ae6691b2a7e297dbe352bb6e544a46a0b453318
John Speck <speckworks@gmail.com>
2020-05-27 10:29:12 -0400

    basic substring recursive function

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Code your solution here!
-
+function printString(myString) {
+    console.log(myString[0], myString[1], myString[2], myString[3], myString[4]);
+  }
+  
+  printString("pizza");
+  
